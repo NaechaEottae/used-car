45e7f4e4f882df7ecd454431cafe4f46c968bc37
NaechaEottae <chea789@naver.com>
2026-09-08 11:23:06 +0900

    메인페이지 리뉴얼

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,11 +6,10 @@ import Image from "next/image";
 import menu from "../public/menu_icon.png";
 import "../app/globals.css";
 import { useEffect, useState } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
   console.log("usePathname", usePathname());
-  console.log("useSearchParams", useSearchParams());
   const [responsiveMenuState, setResponsiveMenuState] =
     useState<boolean>(false);
 
