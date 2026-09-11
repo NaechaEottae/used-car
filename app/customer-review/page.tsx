e201c6c854eac9c26fe4687535b67033892452d4
NaechaEottae <chea789@naver.com>
2026-09-11 13:33:13 +0900

    메인페이지 리뉴얼

--- app/customer-review/page.tsx
+++ app/customer-review/page.tsx
@@ -28,7 +28,7 @@ export default function CustomerReview() {
     <>
       <div className="review-floating">
         <a href="https://blog.naver.com/howsmycar" target="_blank">
-          &nbsp;블로그에서 더 많은 고객리뷰를 확인하세요!&nbsp;
+          &nbsp;&nbsp;블로그에서 더 많은 고객리뷰를 확인하세요!&nbsp;&nbsp;
         </a>
       </div>
       <div className="review-wrap">
